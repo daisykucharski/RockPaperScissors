0741f5d6afaa35d37583f5f6df9b28a278718ed7
daisykucharski <daisy.kucharski@gmail.com>
2021-08-02 15:35:10 -0400

    Basic project complete

---
+++ index.js
@@ -0,0 +1,61 @@
+let playerScore = 0;
+let computerScore = 0;
+
+// Plays five rounds of Rock, Paper, Scissors
+function game() {
+    for (i = 0; i < 5; i++) {
+        const player = promptPlayer();
+        const computer = computerPlay();
+        const msg = playRound(player, computer);
+        console.log(msg);
+    }
+    const finalScores = playerScore + "-" + computerScore;
+    if (playerScore === computerScore) {
+        console.log("It's a tie! " +  finalScores);
+    } else if (playerScore < computerScore) {
+        console.log("The computer wins! " + finalScores);
+    } else {
+        console.log("You win! " + finalScores);
+    }
+}
+
+
+// plays a round of rock paper scissors with the given player and computer selections
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+  const computer = computerSelection.toLowerCase();
+
+  if (player === computer) {
+    return "It's a tie";
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+      ++playerScore;
+      return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+      ++computerScore;
+      return "You lose. " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+// makes the computer's play by randomly picking rock, paper, or scissors
+function computerPlay() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const choice = Math.floor(Math.random() * 3);
+  return options[choice];
+}
+
+// prompts the player to select rock, paper, or scissors. Has them choose again if they didn't pick one of the options
+function promptPlayer() {
+    const choice = prompt("Rock, paper, or scissors?");
+
+    if (!(["rock", "paper", "scissors"].includes(choice.toLowerCase()))) {
+        alert(choice + " is not a valid option, try again");
+        promptPlayer();
+        return;
+    }
+
+    return choice;
+}
