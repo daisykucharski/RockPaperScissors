1adbc07cad08ccf5f3b6e6505f2c5a3b67c770a4
daisykucharski <daisy.kucharski@gmail.com>
2021-08-03 16:14:13 -0400

    Added DOM manipulation

--- index.js
+++ index.js
@@ -1,59 +1,73 @@
 let playerScore = 0;
 let computerScore = 0;
 
-// Plays rock paper scissors until the player or computer reaches 5 points
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        const player = promptPlayer();
-        const computer = computerPlay();
-        const msg = playRound(player, computer);
-        console.log(msg);
+document.querySelector("div.rock-image.button").addEventListener("click", () => {
+    game("rock");
+});
+
+document.querySelector("div.paper-image.button").addEventListener("click", () => {
+    game("paper");
+});
+
+document.querySelector("div.scissors-image.button").addEventListener("click", () => {
+    game("scissors");
+});
+
+function game(player) {
+    if (playerScore === 5 || computerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
     }
-    const finalScores = playerScore + "-" + computerScore;
-    if (playerScore < computerScore) {
-        console.log("The computer wins! " + finalScores);
-    } else {
-        console.log("You win! " + finalScores);
+
+    playRound(player);
+
+    if (playerScore === 5) {
+        document.querySelectorAll("h2")[1].innerText = "You win! Pick rock, paper, or scissors to play again"
+    }
+
+    if (computerScore === 5) {
+        document.querySelectorAll("h2")[1].innerText = "The computer wins! Pick rock, paper, or scissors to play again"
     }
 }
 
-
-// plays a round of rock paper scissors with the given player and computer selections
-function playRound(playerSelection, computerSelection) {
+// plays a round of rock paper scissors with the given player selection and a random computer selection
+function playRound(playerSelection) {
   const player = playerSelection.toLowerCase();
-  const computer = computerSelection.toLowerCase();
+  const computer = computerPlay();
 
+  // Removes previous images from player and computer images and adds the correct images
+  const playerImage = document.querySelectorAll(".result div")[0];
+  playerImage.classList.remove(...playerImage.classList);
+  playerImage.classList.add(player + "-image");
+
+  const computerImage = document.querySelectorAll(".result div")[1];
+  computerImage.classList.remove(...computerImage.classList);
+  computerImage.classList.add(computer + "-image");
+
+  let msg = "";
   if (player === computer) {
-    return "It's a tie";
+    msg = "It's a tie";
   } else if (
     (player === "rock" && computer === "scissors") ||
     (player === "paper" && computer === "rock") ||
     (player === "scissors" && computer === "paper")
   ) {
-      ++playerScore;
-      return "You win! " + playerSelection + " beats " + computerSelection;
+    ++playerScore;
+    msg = "You win this round. " + player.substring(0,1).toUpperCase() + player.substring(1) + " beats " + computer;
   } else {
-      ++computerScore;
-      return "You lose. " + computerSelection + " beats " + playerSelection;
+    ++computerScore;
+    msg = "You lose this round. " + computer.substring(0,1).toUpperCase() + computer.substring(1) + " beats " + player;
   }
+
+  document.querySelectorAll("h2")[2].innerText = "Score: Player " + playerScore + " - Computer " + computerScore;
+  document.querySelectorAll("h2")[1].innerText = msg;
 }
+
 
 // makes the computer's play by randomly picking rock, paper, or scissors
 function computerPlay() {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["rock", "paper", "scissors"];
   const choice = Math.floor(Math.random() * 3);
   return options[choice];
 }
 
-// prompts the player to select rock, paper, or scissors. Has them choose again if they didn't pick one of the options
-function promptPlayer() {
-    const choice = prompt("Rock, paper, or scissors?");
-
-    if (!(["rock", "paper", "scissors"].includes(choice.toLowerCase()))) {
-        alert(choice + " is not a valid option, try again");
-        promptPlayer();
-        return;
-    }
-
-    return choice;
-}
